be4049cc03769f21037dbdba36c4b29aa53e9335
Konstantin Bukley <madundead@gmail.com>
2022-12-01 18:10:26 +0200

    day 1

---
+++ 01/01.rb
@@ -0,0 +1,6 @@
+INPUT = File.readlines('01/input.txt', chomp: true).map(&:to_i)
+
+SUM_GROUP = ->(acc, v) { v != 0 ? [*acc[...-1], acc[-1] += v] : [*acc, 0] }
+
+puts INPUT.inject([0], &SUM_GROUP).sort.last
+puts INPUT.inject([0], &SUM_GROUP).sort[-3..].sum
