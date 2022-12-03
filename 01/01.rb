e950633db07a869e15dba9e68ab168bc28efaa4f
Konstantin Bukley <madundead@gmail.com>
2022-12-03 16:01:23 +0200

    day 1 afterthoughts

--- 01/01.rb
+++ 01/01.rb
@@ -4,3 +4,11 @@ SUM_GROUP = ->(acc, v) { v != 0 ? [*acc[...-1], acc[-1] += v] : [*acc, 0] }
 
 puts INPUT.inject([0], &SUM_GROUP).max
 puts INPUT.inject([0], &SUM_GROUP).sort[-3..].sum
+
+# After r/adventofcode
+# I didn't know about .slice_when
+puts INPUT.slice_when { _1.zero? }.map(&:sum).max
+puts INPUT.slice_when { _1.zero? }.map(&:sum).sort[-3..].sum
+
+# Why doesn't this work?
+# puts INPUT.slice_when(&:zero?).map(&:sum).max
